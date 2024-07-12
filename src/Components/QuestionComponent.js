84d1973a11c101d9753c06d051cfde7a99186d56
orvizz <153991700+orvizz@users.noreply.github.com>
2024-07-12 15:28:00 +0200

    user messages

--- src/Components/QuestionComponent.js
+++ src/Components/QuestionComponent.js
@@ -3,21 +3,29 @@ import { useState } from "react";
 let QuestionComponent = (props) => {
     let { questions, setPoints, points } = props
     let [numberOfQuestion, setNumberOfQuestion] = useState(0);
+    let [message, setMessage] = useState(null);
+
     let answerQuestion = (answer) => {
         if(answer == questions[numberOfQuestion].answer){
             setPoints(points + 1)
-            // msg
+            setMessage("Right answer");
             console.log("Correct");
         } else {
-            // msg 
+            setMessage("Wrong answer")
             console.log("Incorrect");
         }
 
         if ( numberOfQuestion < questions.length - 1){
+            setTimeout(() => {
+                setMessage(null)
+            }, 2000)
             setNumberOfQuestion(numberOfQuestion+1)
         } else{
-
             setTimeout(() => {
+                setMessage("Finnish")
+            }, 2000)
+            setTimeout(() => {
+                setMessage(null)
                 setNumberOfQuestion(0)
                 setPoints(0)
             }, 4000)
@@ -27,12 +35,15 @@ let QuestionComponent = (props) => {
 
     return (
         <div className="question">
+            { message != null &&
+                <h2>{message}</h2>
+            }
             <div>
                 <h2>Question</h2>
                 <p id="questionText">{questions[numberOfQuestion].text}</p>
                 <ul className="options">
-                    {questions[numberOfQuestion].options.map( (o) => 
-                        <li key={o}><button onClick={() => {answerQuestion(questions[numberOfQuestion].options.indexOf(o))}}>{o}</button></li>)}
+                    {questions[numberOfQuestion].options.map( (o, i) => 
+                        <li key={o}><button onClick={() => {answerQuestion(i)}}>{o}</button></li>)}
                 </ul>
             </div>
         </div>
